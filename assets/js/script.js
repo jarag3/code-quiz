c33624ebf3a8c06b4c36b22d0e179eaba4cdb5b3
Jarl Grande <jarl.grande@gmail.com>
2022-04-17 18:39:48 -0700

    quiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,48 @@ const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results')
 const submitButton = document.getElementById('submit')
 
-function buildQuiz(){}
+// Quiz function
+function buildQuiz(){
+    const output = [];
+
+    myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+            // store all answer choices
+            const answers = [];
+
+            for(letter in currentQuestion.answers){
+                // html radio button
+                answers.push(
+                    `<label>
+                        <input type="radio" name="question${questionNumber}" value="${letter}" />
+                        ${letter} :
+                        ${currentQuestion.answers[letter]}
+                    </label>`
+                );
+            }
+            
+            // adds question and its answers to output
+            output.push(    
+                `<div class="question"> ${currentQuestion.question} </div>
+                <div class="answers"> ${answers.join('')} </div>`
+            );
+        }
+    );
+
+    quizContainer.innerHTML = output.join('')
+}
+
+
 
 function showResults(){}
-
+myQuestions.forEach( (currentQuestion, questionNumber) => {
+    // code for each question
+});
 // display quiz instantly
 buildQuiz();
 
 
-// upon submit, show results
+// upon submit, show results (eventlistener)
 submitButton.addEventListener('click', showResults);
 
 
